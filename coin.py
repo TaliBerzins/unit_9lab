d2dc1af3c9f5197e20d7245ec0a9eb30cbd82ff5
TaliBerzins <tali.berzins@outlook.com>
2026-03-14 23:30:43 -0400

    Finished the coin class and added documentation

--- coin.py
+++ coin.py
@@ -5,16 +5,31 @@ To represent a single tossable coin
 03/14/2026
 """
 class Coin:
-
+    """Handles defining the side of the coin for each toss and returning that value
+    
+    Attributes:
+      __sideup : private string value
+   """
+ #Initializer method
     def __init__(self):
-     if random.randint(0, 1) == 0:
+       """Sets up the starting side randomly"""
+       if random.randint(0, 1) == 0:
             self.__sideup = 'Heads'
-     else:
-            self.__sideup = 'Tails' 
+       else:
+            self.__sideup = 'Tails'
+            
 
-    def toss():
-     num = random.randint(0,1)
-     if(num == 0):
-     sideup == "Heads"
+  #Coin toss method 
+    def toss(self):
+     """For each coin toss side is defined to either heads or tails"""
+     if(random.randint(0,1)) == 0:
+       self.__sideup = 'Heads'
+     else:
+       self.__sideup = 'Tails'
+
+    
+    def get_sideup(self):
+       """Returns the sideup variable"""
+       return self.__sideup
        
 
