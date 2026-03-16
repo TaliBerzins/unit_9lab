0fc34683107c14231d20fcb6d2d8153aa1ceb2b3
TaliBerzins <tali.berzins@outlook.com>
2026-03-15 21:37:34 -0400

    Finished main file and added documentation to it

--- lab9_tberzins.py
+++ lab9_tberzins.py
@@ -9,21 +9,41 @@ def main():
   player2 = Player("Player 2")
   print("Player 1 has " + str(player1.get_wallet()) + " coins.")
   print("Player 2 has " + str(player2.get_wallet()) + " coins.")
-
+# Creating loop to run coin toss until player types n for no
   while(input("Do you want to toss the coins? (y/n)\n").upper() == "Y"):
     player1.toss_coin()
     player2.toss_coin()
+
+    #Calling coin side method for each player
     print("Player 1 tossed " + player1.get_coin_side())
     print("Player 2 tossed " + player2.get_coin_side())
-    if(player1.get_coin_side == player2.get_coin_side):
+
+    #Determining who wins and adding coins based off of winner
+    if(player1.get_coin_side() == player2.get_coin_side()):
       print("...It's a Match! Player 1 wins a coin.\n")
-      player1.win_coin
-      player2.lose_coin
+      player1.win_coin()
+      player2.lose_coin()
       print("Player 1 has " + str(player1.get_wallet()) + " coins.")
-      print("Player 2 has " + str(player2.get_wallet()) + " coins.")
+      print("Player 2 has " + str(player2.get_wallet()) + " coins.\n")
 
     else:
       print("...No Match! Player 2 wins a coin.\n")
+      player2.win_coin()
+      player1.lose_coin()
+      print("Player 1 has " + str(player1.get_wallet()) + " coins.")
+      print("Player 2 has " + str(player2.get_wallet()) + " coins.\n")
+
+#Printing final score and determining final winner once games is finished
+  print("--- Final Score ---")
+  print("Player 1: " + str(player1.get_wallet()) + " coins.")
+  print("Player 2: " + str(player2.get_wallet()) + " coins.\n")
+  if player1.get_wallet()>(player2.get_wallet()):
+    print("Player 1 wins!")
+  elif player1.get_wallet()<(player2.get_wallet()):
+        print("Player 2 wins!")
+  else:
+     print("It's a draw!")
+
 
 
 
